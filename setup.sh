fe5d8106f0f7b8ce36bee65c3e465a0f26131ffe
Kazeto Yamamoto <kazeto0513@gmail.com>
2017-03-15 09:29:15 +0900

    Modified setup.sh

--- setup.sh
+++ setup.sh
@@ -1,18 +1,21 @@
 #! /usr/bin/bash
 
 cd `dirname $0`
-
 DIR=`pwd`
+
+git submodule update -i
+
+echo "Setting up dotfiles:"
 DOTFILES=(bash_profile bashrc zshrc emacs.d tmux.conf gitconfig)
 
 for file in ${DOTFILES[@]}
 do
-    if [ -f ${HOME}/.${file} ]; then
+    if [ -e ${HOME}/.${file} ]; then
 	mv ${HOME}/.${file} ${HOME}/.${file}~
     fi
 
-    echo "ln -s ${DIR}/${file} ${HOME}/.${file}"
-    ln -s ${DIR}/${file} ${HOME}/.${file}
+    echo '    "${DIR}/${file}" -> "${HOME}/.${file}"'
+    ln -sf ${DIR}/${file} ${HOME}/.${file}
 done
 
 echo "export DOTFILES_HOME=$DIR" > ~/.dotfiles_profile
