542784b44198dc42e2c595857910e7f4ebf5cc99
Kazeto Yamamoto <kazeto0513@gmail.com>
2017-04-06 16:23:58 +0900

    Modified init-org.el.

--- emacs.d/conf/init-org.el
+++ emacs.d/conf/init-org.el
@@ -1,7 +1,7 @@
 (require 'org-install)
 
 (setq org-todo-keywords
-      '((sequence "SOMEDAY(s)" "TODO(t)" "|" "DONE(d)")))
+      '((sequence "WAIT(w)" "TODO(t)" "|" "DONE(d)")))
 (setq org-log-done 'time)
 
 (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
