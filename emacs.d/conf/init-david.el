4f5d9e25d5abdd8318cbdabdb7bc481cd19f8412
Kazeto Yamamoto <kazeto0513@gmail.com>
2017-05-31 15:34:55 +0900

    Modified init-david.el.

--- emacs.d/conf/init-david.el
+++ emacs.d/conf/init-david.el
@@ -5,20 +5,19 @@
   "David" "View mode for David's input file."
   (setq david-font-lock-keywords
 	'(("#.*$" . font-lock-comment-face)
-	  ("\\(\"[^\"]*\"\\)\\s-*," 1 font-lock-string-face)
-	  ("\\(\"[^\"]*\"\\)\\s-*(" 1 font-lock-string-face)
-	  ("\\(\"[^\"]*\"\\)\\s-*)" 1 font-lock-string-face)
-	  (" \\^ "  . font-lock-constant-face)
-	  (" => "   . font-lock-constant-face)
-	  ("\\<not\\>"  . font-lock-warning-face)
-	  ("!"          . font-lock-warning-face)
-	  ("^problem\\( \\|{\\)"  . font-lock-constant-face)
-	  ("^rule\\( \\|{\\)"     . font-lock-constant-face)
-	  ("^property\\( \\|{\\)" . font-lock-constant-face)
-	  ("\\<\\(observe\\)\\s-*:" 1 font-lock-constant-face)
-	  ("\\<\\(require\\)\\s-*:" 1 font-lock-constant-face)
-	  ("\\<\\(choice\\)\\s-*:"  1 font-lock-constant-face)
-	  (":[^ \t\n(){}]+" . font-lock-comment-face)))
+	  ("\\(\"[^\"]*\"\\)\\s-*[,(){}]" 1 font-lock-string-face)
+	  (" \\^ " . font-lock-keyword-face)
+	  (" => "  . font-lock-keyword-face)
+	  ("\\<not\\>" . font-lock-warning-face)
+	  ("!"         . font-lock-warning-face)
+	  ("^\\(problem\\|rule\\|property\\)\\s-*{" 1 font-lock-type-face)
+	  ("^\\(problem\\|rule\\|property\\)\\s-+\\([^ \t\n{]+\\)\\s-*{"
+	   (1 font-lock-type-face)
+	   (2 font-lock-function-name-face))
+	  ("\\<\\(observe\\|require\\|choice\\)\\s-*{" 1 font-lock-constant-face)
+	  ("\\<\\(transitive\\|asymmetric\\|symmetric\\|right-unique\\|left-unique\\|closed\\|abstract\\):"
+	   (1 font-lock-constant-face))
+	  (":[^ \t\n(){}]+" . font-lock-doc-face)))
   (setq font-lock-defaults
 	  '(david-font-lock-keywords t t nil nil))
   (add-hook 'david-mode-hook 'turn-on-font-lock))
