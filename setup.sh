5c25de71527c7f3ad559445dd1ced25c0720edac
Kazeto Yamamoto <kazeto0513@gmail.com>
2017-09-15 14:29:59 +0900

    Added gdbinit. Updated init-david.el.

--- setup.sh
+++ setup.sh
@@ -6,10 +6,11 @@ DIR=`pwd`
 git submodule update -i
 
 echo "Setting up dotfiles:"
-DOTFILES=(bash_profile bashrc zshrc emacs.d tmux.conf gitconfig)
+DOTFILES=(bash_profile bashrc zshrc emacs.d tmux.conf gitconfig gdbinit)
 
 for file in ${DOTFILES[@]}
 do
+    # Back up old files
     if [ -e ${HOME}/.${file} ]; then
 	mv ${HOME}/.${file} ${HOME}/.${file}~
     fi
